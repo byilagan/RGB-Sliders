ff4fd93c83f182cc5661dad2f76d9951ba53d0af
Bailey Ilagan <ilaganbgm@gmail.com>
2017-06-22 20:35:39 -1000

    Added user defaults

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -21,12 +21,24 @@ class ViewController: UIViewController {
         colorView.layer.borderColor = UIColor.black.cgColor
         colorView.layer.borderWidth = 1
         
+        let defaults = UserDefaults.standard
+        redSlider.value = defaults.float(forKey: "red")
+        greenSlider.value = defaults.float(forKey: "green")
+        blueSlider.value = defaults.float(forKey: "blue")
+        
         updateBackgroundColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "openColor") {
+            let newViewController = segue.destination
+            newViewController.view.backgroundColor = colorView.backgroundColor
+        }
     }
 
     @IBAction func updateBackgroundColor() {
@@ -35,6 +47,12 @@ class ViewController: UIViewController {
         let bVal = blueSlider.value
         
         colorView.backgroundColor = UIColor(colorLiteralRed: rVal, green: gVal, blue: bVal, alpha: 1)
+        
+        let defaults = UserDefaults.standard  //1
+        defaults.set(redSlider.value, forKey: "red")   //2
+        defaults.set(greenSlider.value, forKey: "green")
+        defaults.set(blueSlider.value, forKey: "blue")
+        defaults.synchronize()    //3
     }
     
 }
